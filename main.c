d2299de31116dca0be889b8eb9f51b566ff219cb
Nikolaj <karpicnikolaj@gmail.com>
2021-05-04 14:53:03 +0200

    Refactored code into other files, changed logic for left state

--- main.c
+++ main.c
@@ -6,18 +6,20 @@
  */
 
 
-#include <stdio.h>
-#include <stdlib.h>
-#include<p30fxxxx.h>
+// #include <stdio.h>
+// #include <stdlib.h>
+//#include<p30fxxxx.h>
 
-
+#include "timer.h"
+#include "uart.h"
+#include "movement.h"
 _FOSC(CSW_FSCM_OFF & XT_PLL4);//instruction takt je isti kao i kristal
 _FWDT(WDT_OFF);
 
-#define TMR1_period 10 /*  Fosc = 10MHz,
-					          1/Fosc = 0.1us !!!, 0.1us * 10 = 1us  */
-#define TMR3_period 58800 /*  Fosc = 10MHz,
-					          1/Fosc = 0.1us !!!, 0.1us * 65000 = 6.5ms  */
+// #define TMR1_period 10 /*  Fosc = 10MHz,
+// 					          1/Fosc = 0.1us !!!, 0.1us * 10 = 1us  */
+// #define TMR3_period 58800 /*  Fosc = 10MHz,
+// 					          1/Fosc = 0.1us !!!, 0.1us * 65000 = 6.5ms  */
 #define trig2 LATDbits.LATD0 
 #define echo2 PORTDbits.RD1
 #define trig1 LATBbits.LATB0
@@ -28,11 +30,11 @@ _FWDT(WDT_OFF);
 #define M2_1 LATBbits.LATB5 
 #define M2_2 LATBbits.LATB6 
 
-#define servo_period 780 //sa preskalerom od 256 ova vrednost podesava frekvenciju pwm-a da bude 50Hz
-#define motor_speed OC4RS //output compare na pinu RD3
+// #define servo_period 780 //sa preskalerom od 256 ova vrednost podesava frekvenciju pwm-a da bude 50Hz
+// #define motor_speed OC4RS //output compare na pinu RD3
 
-unsigned int stoperica = 0;
-unsigned char tempRX;
+//unsigned int stoperica = 0;
+// unsigned char tempRX;
 
 unsigned int distance_fwd;
 unsigned int distance_right;
@@ -52,257 +54,258 @@ unsigned int prefered_distance_right = 17;
 unsigned int hysteresis_fwd = 5;
 unsigned int hysteresis_right = 1;
 
-enum tank_state{ idle_s, fwd_s, left_s, right_s, bck_s} state;
+enum tank_state{ idle_s, fwd_s, left_s, right_s, bck_s} current_state;
+tank_state next_state;
 
-void Init_T1(void)
-{
-	TMR1 = 0;
-	PR1 = TMR1_period;
+// void Init_T1(void)
+// {
+// 	TMR1 = 0;
+// 	PR1 = TMR1_period;
 	
-	T1CONbits.TCS = 0; // 0 = Internal clock (FOSC/4)
-	//IPC1bits.T1IP = 3 // T1 interrupt pririty (0-7)
-	//SRbits.IPL = 3; // CPU interrupt priority is 3(11)
-	IFS0bits.T1IF = 0; // clear interrupt flag
-	IEC0bits.T1IE = 1; // enable interrupt
+// 	T1CONbits.TCS = 0; // 0 = Internal clock (FOSC/4)
+// 	//IPC1bits.T1IP = 3 // T1 interrupt pririty (0-7)
+// 	//SRbits.IPL = 3; // CPU interrupt priority is 3(11)
+// 	IFS0bits.T1IF = 0; // clear interrupt flag
+// 	IEC0bits.T1IE = 1; // enable interrupt
 
-	T1CONbits.TON = 1; // T1 on 
-}
+// 	T1CONbits.TON = 1; // T1 on 
+// }
 
-void Init_T2(void)
-{
-        PR2=servo_period;//odredjuje frekvenciju po formuli
-        T2CONbits.TCKPS=0b11; //preskaler 1:256
-        //IFS0bits.T2IF = 0; // clear interrupt flag
-        //IEC0bits.T2IE = 1; // enable interrupt
-        T2CONbits.TON=1;//ukljucujemo timer koji koristi
+// void Init_T2(void)
+// {
+//         PR2=servo_period;//odredjuje frekvenciju po formuli
+//         T2CONbits.TCKPS=0b11; //preskaler 1:256
+//         //IFS0bits.T2IF = 0; // clear interrupt flag
+//         //IEC0bits.T2IE = 1; // enable interrupt
+//         T2CONbits.TON=1;//ukljucujemo timer koji koristi
         
-        OC4RS=20;//postavimo pwm
-        OC4R=1000;//inicijalni pwm pri paljenju samo
-        OC4CONbits.OCM = 0b110; //output ccompare mudule for pwm
-        OC4RS = 0;
+//         OC4RS=20;//postavimo pwm
+//         OC4R=1000;//inicijalni pwm pri paljenju samo
+//         OC4CONbits.OCM = 0b110; //output ccompare mudule for pwm
+//         OC4RS = 0;
         
-        IFS0bits.T2IF = 0; // clear interrupt flag
-        IEC0bits.T2IE = 1; // enable interrupt
+//         IFS0bits.T2IF = 0; // clear interrupt flag
+//         IEC0bits.T2IE = 1; // enable interrupt
         
-}
+// }
 
-void Init_T3(void)
-{
-	TMR3 = 0;
-	PR3 = TMR3_period;
+// void Init_T3(void)
+// {
+// 	TMR3 = 0;
+// 	PR3 = TMR3_period;
 	
-	T3CONbits.TCS = 0; // 0 = Internal clock (FOSC/4)
-	//IPC1bits.T3IP = 3 // T3 interrupt pririty (0-7)
-	//SRbits.IPL = 3; // CPU interrupt priority is 3(11)
-	IFS0bits.T3IF = 0; // clear interrupt flag
-	IEC0bits.T3IE = 1; // enable interrupt
+// 	T3CONbits.TCS = 0; // 0 = Internal clock (FOSC/4)
+// 	//IPC1bits.T3IP = 3 // T3 interrupt pririty (0-7)
+// 	//SRbits.IPL = 3; // CPU interrupt priority is 3(11)
+// 	IFS0bits.T3IF = 0; // clear interrupt flag
+// 	IEC0bits.T3IE = 1; // enable interrupt
 
-	T3CONbits.TON = 1; // T3 on 
-}
+// 	T3CONbits.TON = 1; // T3 on 
+// }
 
-void __attribute__ ((__interrupt__)) _T1Interrupt(void){//svakih 1us
+// void __attribute__ ((__interrupt__)) _T1Interrupt(void){//svakih 1us
     
-    TMR1 =0;
-    stoperica++;
-    IFS0bits.T1IF = 0; 
+//     TMR1 =0;
+//     stoperica++;
+//     IFS0bits.T1IF = 0; 
     
-}
+// }
 
-void __attribute__ ((__interrupt__)) _T2Interrupt(void) // svakih 20ms
-{//tajmer 2 radi na frekvenciji od 50Hz
-    TMR2 = 0;
-    IFS0bits.T2IF = 0; 
+// void __attribute__ ((__interrupt__)) _T2Interrupt(void) // svakih 20ms
+// {//tajmer 2 radi na frekvenciji od 50Hz
+//     TMR2 = 0;
+//     IFS0bits.T2IF = 0; 
        
-}
-unsigned int tmr3_counter = 0;
-void __attribute__ ((__interrupt__)) _T3Interrupt(void){ //svakih 6.5ms
+// }
+// unsigned int tmr3_counter = 0;
+// void __attribute__ ((__interrupt__)) _T3Interrupt(void){ //svakih 6.5ms
     
-    TMR3 =0;
+//     TMR3 =0;
     
-    tmr3_counter++;
-    IFS0bits.T3IF = 0; 
-}
+//     tmr3_counter++;
+//     IFS0bits.T3IF = 0; 
+// }
 
-void set_speed(unsigned int percent){
-    unsigned int temp = 780*percent;
-    temp/=100;
-    motor_speed = temp;
-}
+// void set_speed(unsigned int percent){
+//     unsigned int temp = 780*percent;
+//     temp/=100;
+//     motor_speed = temp;
+// }
 
-void delay_us (int vreme)//funkcija za kasnjenje u milisekundama
-	{
-        T1CONbits.TON = 1;
-		stoperica = 0;
-		while(stoperica < vreme);
-        T1CONbits.TON = 0;
-	}
+// void delay_us (int vreme)//funkcija za kasnjenje u milisekundama
+// 	{
+//         T1CONbits.TON = 1;
+// 		stoperica = 0;
+// 		while(stoperica < vreme);
+//         T1CONbits.TON = 0;
+// 	}
 
-unsigned int ultrasonic_sensor_right(){
-    unsigned int distance;
+// unsigned int ultrasonic_sensor_right(){
+//     unsigned int distance;
     
-    TMR3=0;
-    LATBbits.LATB0=1;//triger=1
-    delay_us(10);
-    LATBbits.LATB0 = 0;
-    tmr3_counter = 0;
-    while(!PORTBbits.RB1);
-    T3CONbits.TON = 1;
-    while(PORTBbits.RB1);
-    T3CONbits.TON = 0;
+//     TMR3=0;
+//     LATBbits.LATB0=1;//triger=1
+//     delay_us(10);
+//     LATBbits.LATB0 = 0;
+//     tmr3_counter = 0;
+//     while(!PORTBbits.RB1);
+//     T3CONbits.TON = 1;
+//     while(PORTBbits.RB1);
+//     T3CONbits.TON = 0;
     
-    return TMR3/588 + tmr3_counter*100;
-}
+//     return TMR3/588 + tmr3_counter*100;
+// }
 
-unsigned int ultrasonic_sensor_fwd(){
+// unsigned int ultrasonic_sensor_fwd(){
     
-    TMR3=0;
-    trig2 = 1;
-    delay_us(10);
-    trig2 = 0;
-    tmr3_counter = 0;
-    while(!echo2);
-    T3CONbits.TON = 1;
-    while(echo2);
-    T3CONbits.TON = 0;
+//     TMR3=0;
+//     trig2 = 1;
+//     delay_us(10);
+//     trig2 = 0;
+//     tmr3_counter = 0;
+//     while(!echo2);
+//     T3CONbits.TON = 1;
+//     while(echo2);
+//     T3CONbits.TON = 0;
     
-    return TMR3/588+100*tmr3_counter;
-}
+//     return TMR3/588+100*tmr3_counter;
+// }
 
-void initUART1(void)
-{
-    U1BRG=0x0040;//ovim odredjujemo baudrate
+// void initUART1(void)
+// {
+//     U1BRG=0x0040;//ovim odredjujemo baudrate
 
-    U1MODEbits.ALTIO=1;//biramo koje pinove koristimo za komunikaciju osnovne ili alternativne
+//     U1MODEbits.ALTIO=1;//biramo koje pinove koristimo za komunikaciju osnovne ili alternativne
 
-    IEC0bits.U1RXIE=1;//omogucavamo rx1 interupt
+//     IEC0bits.U1RXIE=1;//omogucavamo rx1 interupt
 
-    U1STA&=0xfffc;
+//     U1STA&=0xfffc;
 
-    U1MODEbits.UARTEN=1;//ukljucujemo ovaj modul
+//     U1MODEbits.UARTEN=1;//ukljucujemo ovaj modul
 
-    U1STAbits.UTXEN=1;//ukljucujemo predaju
-}
+//     U1STAbits.UTXEN=1;//ukljucujemo predaju
+// }
 
-void __attribute__((__interrupt__)) _U1RXInterrupt(void) 
-{
-    IFS0bits.U1RXIF = 0;
-    tempRX=U1RXREG;
+// void __attribute__((__interrupt__)) _U1RXInterrupt(void) 
+// {
+//     IFS0bits.U1RXIF = 0;
+//     tempRX=U1RXREG;
 
-} 
+// } 
 
-/*********************************************************************
-* Ime funkcije      : WriteUART1                                     *
-* Opis              : Funkcija upisuje podatak u registar 			 *
-* 					  za slanje podataka U1TXREG,   				 *
-* Parametri         : unsigned int data-podatak koji treba poslati   *
-* Povratna vrednost : Nema                                           *
-*********************************************************************/
+// /*********************************************************************
+// * Ime funkcije      : WriteUART1                                     *
+// * Opis              : Funkcija upisuje podatak u registar 			 *
+// * 					  za slanje podataka U1TXREG,   				 *
+// * Parametri         : unsigned int data-podatak koji treba poslati   *
+// * Povratna vrednost : Nema                                           *
+// *********************************************************************/
 
-void WriteUART1(unsigned int data)
-{
-	  while(!U1STAbits.TRMT);
+// void WriteUART1(unsigned int data)
+// {
+// 	  while(!U1STAbits.TRMT);
 
-    if(U1MODEbits.PDSEL == 3)
-        U1TXREG = data;
-    else
-        U1TXREG = data & 0xFF;
-}
+//     if(U1MODEbits.PDSEL == 3)
+//         U1TXREG = data;
+//     else
+//         U1TXREG = data & 0xFF;
+// }
 
-void WriteStringToUART1(unsigned char * str){
-    unsigned int k = 0;
-    while(*(str+k) != 0x00){
-        WriteUART1(*(str+k));
-        k++;
-    }
-}
-void check_direction(){ //kontrola H mosta za smer motora
+// void WriteStringToUART1(unsigned char * str){
+//     unsigned int k = 0;
+//     while(*(str+k) != 0x00){
+//         WriteUART1(*(str+k));
+//         k++;
+//     }
+// }
+// void check_direction(){ //kontrola H mosta za smer motora
 
     
-    if(fwd == 1){ //1010 je za smer napred
-        M1_1 = 1;
-        M1_2 = 0;
-        M2_1 = 1;
-        M2_2 = 0;
+//     if(fwd == 1){ //1010 je za smer napred
+//         M1_1 = 1;
+//         M1_2 = 0;
+//         M2_1 = 1;
+//         M2_2 = 0;
         
-        return 0;
-    }
-    if(bck == 1){// 0101 je smer za nazad
-        M1_1 = 0;
-        M1_2 = 1;
-        M2_1 = 0;
-        M2_2 = 1;
+//         return 0;
+//     }
+//     if(bck == 1){// 0101 je smer za nazad
+//         M1_1 = 0;
+//         M1_2 = 1;
+//         M2_1 = 0;
+//         M2_2 = 1;
         
-        return 0;
-    }
+//         return 0;
+//     }
     
-    if(left == 1){//Smer za levo je 1001
-        M1_1 = 1;
-        M1_2 = 0;
-        M2_1 = 0;
-        M2_2 = 1;
+//     if(left == 1){//Smer za levo je 1001
+//         M1_1 = 1;
+//         M1_2 = 0;
+//         M2_1 = 0;
+//         M2_2 = 1;
         
-        return 0;
-    }if(right == 1){ //smer za desno je 0110
-        M1_1 = 0;
-        M1_2 = 1;
-        M2_1 = 1;
-        M2_2 = 0;
+//         return 0;
+//     }if(right == 1){ //smer za desno je 0110
+//         M1_1 = 0;
+//         M1_2 = 1;
+//         M2_1 = 1;
+//         M2_2 = 0;
         
-        return 0;
-    }if(fwd_right == 1){
-        M1_1 = 0;
-        M1_2 = 0;
-        M2_1 = 1;
-        M2_2 = 0;
+//         return 0;
+//     }if(fwd_right == 1){
+//         M1_1 = 0;
+//         M1_2 = 0;
+//         M2_1 = 1;
+//         M2_2 = 0;
         
-        return 0;
-    }if(fwd_left == 1){
-        M1_1 = 1;
-        M1_2 = 0;
-        M2_1 = 0;
-        M2_2 = 0;
+//         return 0;
+//     }if(fwd_left == 1){
+//         M1_1 = 1;
+//         M1_2 = 0;
+//         M2_1 = 0;
+//         M2_2 = 0;
         
-        return 0;
-    }
+//         return 0;
+//     }
     
-    M1_1 = 0;//motori su ugaseni za 0000
-    M1_2 = 0;
-    M2_1 = 0;
-    M2_2 = 0;
-}
+//     M1_1 = 0;//motori su ugaseni za 0000
+//     M1_2 = 0;
+//     M2_1 = 0;
+//     M2_2 = 0;
+// }
 unsigned int brojac_desno = 0, br;
 
-void stop(){
-    fwd = 0;
-    bck = 0;
-    left = 0;
-    right = 0;
-    fwd_left = 0;
-    fwd_right = 0;
-}
+// void stop(){
+//     fwd = 0;
+//     bck = 0;
+//     left = 0;
+//     right = 0;
+//     fwd_left = 0;
+//     fwd_right = 0;
+// }
 
-void check_obstacles(){
+// void check_obstacles(){
     
     
-    char buffer[20];
+//     char buffer[20];
     
-    distance_fwd = ultrasonic_sensor_fwd();
-    distance_right = ultrasonic_sensor_right();
+//     distance_fwd = ultrasonic_sensor_fwd();
+//     distance_right = ultrasonic_sensor_right();
 
-    sprintf(buffer, "desno: %dcm\n", distance_right);
-    WriteStringToUART1(buffer);
+//     sprintf(buffer, "desno: %dcm\n", distance_right);
+//     WriteStringToUART1(buffer);
 
-    sprintf(buffer, "napred: %dcm\n", distance_fwd);
-    //WriteStringToUART1(buffer);
+//     sprintf(buffer, "napred: %dcm\n", distance_fwd);
+//     //WriteStringToUART1(buffer);
 
     
-   // if(distance_right == 27 || distance_right == 28) fwd_right = 1;
+//    // if(distance_right == 27 || distance_right == 28) fwd_right = 1;
     
     
-}
+// }
 
 void print_state(){
-    switch(state){
+    switch(current_state){
         case idle_s:
             WriteStringToUART1("State: IDLE\n");
             break;
@@ -358,25 +361,26 @@ int main(void) {
     
     while(1){
         check_obstacles();
-        
-        switch (state){
+        current_state = next_state;
+
+        switch (current_state){
             case idle_s:
                 fwd = 1;
                 if (distance_right <= prefered_distance_right + hysteresis_right){
                     if (distance_right >= prefered_distance_right - hysteresis_right ){
                         stop();
                         fwd = 1;
-                        state = fwd_s;
+                        next_state = fwd_s;
                         break;
                     }else {
                         stop();
                         left = 1;
-                        break;
+                        //break;
                     }
                 } else {
                     stop();
                     right = 1;
-                    break;
+                    //break;
                 }
                 break;
             case fwd_s:
@@ -397,7 +401,7 @@ int main(void) {
                 }
                 if (distance_fwd <= prefered_distance_fwd - hysteresis_fwd){
                     //if(distance_fwd >= prefered_distance_fwd - hysteresis_fwd){
-                        state = left_s; 
+                        next_state = left_s; 
                         break;
                     /*}else{
                     stop();
@@ -407,7 +411,7 @@ int main(void) {
                     }*/
                 }
                 if (distance_right >= turning_threshold){
-                    state = right_s;
+                    next_state = right_s;
                     break;
                 }
                 break;
@@ -415,16 +419,17 @@ int main(void) {
                 stop();
                 left = 1;// treba fiksno vreme za rotaciju od 90 stepeni porm. unsigned int turning_time_left;
                 check_direction();
-                for (i = 0; i<turning_time_left; i++){
+                while(!(distance_right <= prefered_distance_right + hysteresis_right && distance_right >= prefered_distance_right - hysteresis_right)){
+                //for (i = 0; i<turning_time_left; i++){
                     delay_us(10000);
                 }
-                stop();
-                fwd = 1;
-                check_direction();
-                for (i = 0; i<turning_time_right+10; i++){
-                    delay_us(10000);
-                }
-                state = fwd_s;
+                //stop();
+                // fwd = 1;
+                // check_direction();
+                // for (i = 0; i<turning_time_right+10; i++){
+                //     delay_us(10000);
+                // }
+                next_state = fwd_s;
                 break;
             case right_s:
                 stop();
@@ -439,13 +444,13 @@ int main(void) {
                 for (i = 0; i<turning_time_right; i++){
                     delay_us(10000);
                 }
-                state = fwd_s;
+                next_state = fwd_s;
                 break;
             case bck_s:
                 stop();
                 bck = 1;
                 if(distance_fwd >= prefered_distance_fwd - hysteresis_fwd){
-                    state = fwd_s;
+                    next_state = fwd_s;
                     break;
                 }
                 break;
